b30a54358eef371d85dc17aade1505c865844ff4
lampawood <lampawood7703@gmail.com>
2020-11-08 16:51:24 +0300

    Updates

---
+++ precache-manifest.937b1118c316a5834a83c7dadf30df54.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6543ad46cd4924d0cdef2a4145e4e35e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ecb7591b878026401b5",
+    "url": "/static/css/main.d75ded4c.chunk.css"
+  },
+  {
+    "revision": "bacaac0a1e8f07d70907",
+    "url": "/static/js/2.a5c7dd5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5c7dd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecb7591b878026401b5",
+    "url": "/static/js/main.3977d87f.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "9e35f85831d68caadf15de3f2c7450fd",
+    "url": "/static/media/add_button.9e35f858.svg"
+  },
+  {
+    "revision": "33286c6dc65271f0de96dbd3742f11a6",
+    "url": "/static/media/add_button2.33286c6d.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/static/media/close.5df32a06.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "/static/media/delete.4220563a.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "/static/media/edit_button.b05083e5.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/static/media/heart.83a46f45.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/static/media/heart_liked.5bbb05f7.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/static/media/logo.c6f11019.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/static/media/pencil.d476a8b8.svg"
+  }
+]);
